2f5b8f3eb62cd4143ff9df82b6f48b2a210ee689
awesomeme01 <shabdan.abjaparov.official@gmail.com>
2021-03-02 20:52:00 +0600

    changed main class again

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 
 public class Main{//мир
 
-
-
     public static void main(String[] args) {
         Random rand = new Random();
         int[] numbers = new int[10];
+        //новый коммент
 
         for(int i = 0; i < numbers.length; i++){
             numbers[i] = rand.nextInt(6);//0 - 5
